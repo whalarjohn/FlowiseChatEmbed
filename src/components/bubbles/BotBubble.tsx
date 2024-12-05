5b0d5063114611cfeaf6e942d6404dfc2682f8db
John Williams <john@whalar.com>
2024-12-05 16:13:44 +0100

    asdasasdasdasd

--- src/components/bubbles/BotBubble.tsx
+++ src/components/bubbles/BotBubble.tsx
@@ -380,7 +380,7 @@ export const BotBubble = (props: Props) => {
               style={{
                 'background-color': props.backgroundColor ?? defaultBackgroundColor,
                 color: props.textColor ?? defaultTextColor,
-                'border-radius': '6px',
+                'border-radius': '20px',
                 'font-size': props.fontSize ? `${props.fontSize}px` : `${defaultFontSize}px`,
               }}
             />
