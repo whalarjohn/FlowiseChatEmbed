fc77160e4001be9eb016a33309d5347dd548c3c0
John Williams <john@whalar.com>
2024-12-12 16:11:41 +0100

    fixes for starter prompts

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -12,13 +12,14 @@ export const StarterPromptBubble = (props: Props) => (
       onClick={() => props.onPromptClick?.()}
     >
       <span
-        class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+        class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble starter-prompt"
         data-testid="host-bubble"
         style={{
-          width: 'max-content',
-          'font-size': props.starterPromptFontSize ? `${props.starterPromptFontSize}px` : '15px', // Convert to string with unit
+          width: '100%',
+          'font-size': props.starterPromptFontSize ? `${props.starterPromptFontSize}px!important` : '15px!important', // Convert to string with unit
           'border-radius': '15px',
           cursor: 'pointer',
+          'background-color': 'var(--color-primary)',
         }}
       >
         {props.prompt}
